987c951db4c101b57187f6040c1d91c3de81b094
foysal <foysalf652@gmail.com>
2022-04-21 18:46:18 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./dist/**/*.{html,js}",
+    './index.html',
+    './pricing.html',
+   
+    
+],
+  theme: {
+    extend: {
+      boxShadow: {
+        'inner-input': 'inset -3px -3px 9px 1px #fff, inset 3px 5px 8px -6px black',
+        'date': "-2px -2px 9px 2px #fff, 3px 3px 9px -3px black"
+      }
+    },
+  },
+  plugins: [],
+}
